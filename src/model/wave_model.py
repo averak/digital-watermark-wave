583bc7b4ce8a171325c7ee68dc08784c58bac403
averak <abe12@mccc.jp>
2021-12-25 01:32:17 +0900

    [feat] define wave model

---
+++ src/model/wave_model.py
@@ -0,0 +1,23 @@
+from pydantic import BaseModel, Field
+from config.wave_config import WaveConfig
+
+
+class WaveModel(BaseModel):
+    """
+    音声モデル
+    """
+
+    rate: int = Field(default=WaveConfig.RATE)
+    """
+    サンプリングレート[Hz]
+    """
+
+    length: float
+    """
+    音声の長さ[s]
+    """
+
+    content: list[int]
+    """
+    音声データ
+    """
